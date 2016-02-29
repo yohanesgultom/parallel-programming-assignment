94fe52b7cda7a3b8314597c1571b7fca661cc199
Yohanes Gultom <yohanes.gultom@gmail.com>
2016-02-29 18:22:25 +0700

    update qsub

--- problem1/mm_cannon.fasilkom.sh
+++ problem1/mm_cannon.fasilkom.sh
@@ -10,18 +10,8 @@ exe="$name.o"
 out="$name.fasilkom.result"
 clear && mpicc -o $exe $prog -lm &&
 echo "NP\tData\tComm Time\tProcess Time" > $out &&
-mpirun -hostfile ~/mpi_hostfile -np 2 $exe 500 >> $out &&
-mpirun -hostfile ~/mpi_hostfile -np 2 $exe 500 >> $out &&
-mpirun -hostfile ~/mpi_hostfile -np 2 $exe 500 >> $out &&
-mpirun -hostfile ~/mpi_hostfile -np 2 $exe 500 >> $out &&
-mpirun -hostfile ~/mpi_hostfile -np 2 $exe 500 >> $out &&
 mpirun -hostfile ~/mpi_hostfile -np 4 $exe 500 >> $out &&
 mpirun -hostfile ~/mpi_hostfile -np 4 $exe 500 >> $out &&
 mpirun -hostfile ~/mpi_hostfile -np 4 $exe 500 >> $out &&
 mpirun -hostfile ~/mpi_hostfile -np 4 $exe 500 >> $out &&
-mpirun -hostfile ~/mpi_hostfile -np 4 $exe 500 >> $out &&
-mpirun -hostfile ~/mpi_hostfile -np 8 $exe 500 >> $out &&
-mpirun -hostfile ~/mpi_hostfile -np 8 $exe 500 >> $out &&
-mpirun -hostfile ~/mpi_hostfile -np 8 $exe 500 >> $out &&
-mpirun -hostfile ~/mpi_hostfile -np 8 $exe 500 >> $out &&
-mpirun -hostfile ~/mpi_hostfile -np 8 $exe 500 >> $out
+mpirun -hostfile ~/mpi_hostfile -np 4 $exe 500 >> $out
