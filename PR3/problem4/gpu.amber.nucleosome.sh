e212452ed6527d9d22270b62a6632e24a07a033c
Yohanes Gultom <yohanes.gultom@gmail.com>
2016-05-23 14:11:51 +0700

    update

--- PR3/problem4/gpu.amber.nucleosome.sh
+++ PR3/problem4/gpu.amber.nucleosome.sh
@@ -5,5 +5,5 @@ echo "NUCLEOSOME_PRODUCTION - 25,095 atoms GB"
 echo "---------------------------------------"
 cd Amber_GPU_Benchmark_Suite/GB/nucleosome
 export CUDA_VISIBLE_DEVICES=0
-$AMBERHOME/bin/pmemd.cuda -O -o mdout.GPU -inf mdinfo.GPU -x mdcrd.GPU -r restrt.GPU -ref inpcrd -i mdin.GPU
+$AMBERHOME/bin/pmemd.cuda -O -o mdout.GPU -inf mdinfo.GPU -x mdcrd.GPU -r restrt.GPU -ref inpcrd
 tail -100f mdinfo.GPU
